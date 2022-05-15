63863aa3e5631e44eb74ac93f4bddb5441f9afc3
thedevluffy <thedevluffy@gmail.com>
2022-05-15 16:54:12 +0900

    init project

---
+++ src/main/java/com/zolmeet/zolmeet/ZolmeetApplication.java
@@ -0,0 +1,13 @@
+package com.zolmeet.zolmeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZolmeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZolmeetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zolmeet/zolmeet/ZolmeetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zolmeet.zolmeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZolmeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
